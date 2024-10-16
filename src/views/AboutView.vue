a6cbca24ca3250c93cf99a0188bd08a958cafa05
soyUnaGata <fallen.snitch@gmail.com>
2024-10-16 21:50:20 +0300

    init project

---
+++ src/views/AboutView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="about bg-blue-500 h-0 w-2">
+    <h1 class="pd-3">Heeloo</h1>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
