c9a612a4db09160687b78adb83026a31bea387c2
soyUnaGata <fallen.snitch@gmail.com>
2024-10-17 21:14:31 +0300

    remove shadow

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,25 +1,17 @@
 <template>
   <div class="about flex bg-blue-500 h-0 w-2 flex-col">
     <h1 class="pd-3">Heeloo</h1>
-    <div v-for="(group, index) in uniqueSubcategories" :key="index">
-      <h2>{{ group.name }}</h2>
-      <ul>
-        <li v-for="(value, idx) in group.subcategories" :key="idx">
-          {{ value }}
-        </li>
-      </ul>
-    </div>
   </div>
-  <div v-for="(group, index) in uniqueSubcategories" :key="index">
-  <Popover class="relative bg-orange-500">
+  <div class="flex" v-for="(group, index) in uniqueSubcategories" :key="index">
+  <Popover class="relative bg-orange-500 w-100 w-screen flex-auto overflow-hidden rounded-3xl bg-white text-sm leading-6 shadow-lg ring-1">
     <PopoverButton class="inline-flex items-center gap-x-1 text-sm font-semibold leading-6 text-gray-900">
       <span>{{group.name}}</span>
       <ChevronDownIcon class="h-5 w-5" aria-hidden="true" />
     </PopoverButton>
 
     <transition enter-active-class="transition ease-out duration-200" enter-from-class="opacity-0 translate-y-1" enter-to-class="opacity-100 translate-y-0" leave-active-class="transition ease-in duration-150" leave-from-class="opacity-100 translate-y-0" leave-to-class="opacity-0 translate-y-1">
-      <PopoverPanel class="absolute left-1/2 z-10 mt-5 flex w-screen max-w-max -translate-x-1/2 px-4">
-        <div class="w-screen max-w-md flex-auto overflow-hidden rounded-3xl bg-white text-sm leading-6 shadow-lg ring-1 ring-gray-900/5">
+      <PopoverPanel class="left-1/2 z-10 mt-5 flex w-screen max-w-max -translate-x-1/2 px-4">
+        <div class="w-screen max-w-md flex-auto overflow-hidden rounded-3xl bg-white text-sm leading-6 ring-gray-900/5">
           <div class="p-4">
             <div v-for="(value, idx) in group.subcategories" :key="idx" class="group relative flex gap-x-6 rounded-lg p-4 hover:bg-gray-50">
               <div class="mt-1 flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
