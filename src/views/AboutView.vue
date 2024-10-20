08357a20ea2b2be63e137d476a86b3207f670360
soyUnaGata <fallen.snitch@gmail.com>
2024-10-20 15:25:57 +0300

    amend link and template

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,28 +1,27 @@
 <template>
-  <div class="about flex bg-blue-500 h-0 w-2 flex-col">
-    <h1 class="pd-3">Heeloo</h1>
-  </div>
-  <div class="flex" v-for="(group, index) in uniqueSubcategories" :key="index">
-  <Popover class="relative shadow-lg ring-1 ring-gray-900/5 w-100 w-screen flex-auto overflow-hidden rounded-3xl bg-white text-sm leading-6 shadow-lg ring-1">
+  
+   <div class="flex" v-for="(filter, index) in filters" :key="index">
+ <Popover class="relative shadow-lg ring-1 ring-gray-900/5 w-100 w-screen flex-auto overflow-hidden rounded-3xl bg-white text-sm leading-6 shadow-lg ring-1">
     <PopoverButton class="dropdown__filter inline-flex h-12 items-center gap-x-1 text-sm font-semibold leading-6 text-gray-900 pl-3">
-      <span>{{group.name}}</span>
+      <span>{{filter.name}}</span>
+      <span>{{ group }}</span>
       <ChevronDownIcon class="h-5 w-5" aria-hidden="true" />
     </PopoverButton>
-    <a href="./EditView.vue">Edit</a>
-    <RouterLink to="/filter/edit">Edit</RouterLink>
+    <a :href="`/filter/edit/${filter.id}`">Edit</a>
+    <RouterLink :to="`/filter/edit/${filter.id}`">Edit</RouterLink>
     <span>Delete</span>
 
     <transition enter-active-class="transition ease-out duration-200" enter-from-class="opacity-0 translate-y-1" enter-to-class="opacity-100 translate-y-0" leave-active-class="transition ease-in duration-150" leave-from-class="opacity-100 translate-y-0" leave-to-class="opacity-0 translate-y-1">
       <PopoverPanel class="left-1/2 z-10 mt-5 flex w-screen max-w-max px-4">
         <div class="w-screen max-w-md flex-auto overflow-hidden rounded-3xl bg-white text-sm leading-6 ring-gray-900/5">
           <div class="p-4">
-            <div v-for="(value, idx) in group.subcategories" :key="idx" class="group relative flex gap-x-6 rounded-lg p-4 hover:bg-gray-50">
+            <div v-for="(value, idx) in filter.filter_values" :key="idx" class="group relative flex gap-x-6 rounded-lg p-4 hover:bg-gray-50">
               <div class="mt-1 flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
                 <component  class="h-6 w-6 text-gray-600 group-hover:text-indigo-600" aria-hidden="true" />
               </div>
               <div>
-                <a :href="value" class="font-semibold text-gray-900">
-                  {{ value }}
+                <a :href="value.value" class="font-semibold text-gray-900">
+                  {{ value.value }}
                   <span class="absolute inset-0" />
                 </a>
                 <p class="mt-1 text-gray-600">{{ }}</p>
@@ -30,48 +29,50 @@
             </div>
           </div>
         </div>
-      </PopoverPanel>
+      </PopoverPanel> 
     </transition>
-  </Popover>
-  </div>
+  </Popover> 
+  </div> 
 </template>
 
 <script setup>
 import axios from "axios";
-import { ref, onMounted, computed } from "vue";
+import groupBy from 'lodash/groupBy';
+import { ref, onMounted, computed, watch, } from "vue";
 
-const subcategories = ref(); 
+const filters = ref(); 
+const filterId = ref(null);
+const filter = ref(null);
+const route = ref(null);
 
-const fetchSubcategories = async () => {
+
+
+
+const fetchFilters = async () => {
   try {
-    const response = await axios.get('https://aerobay.onrender.com/api/subcategory');
-    subcategories.value = response.data;
+    const response = await axios.get('https://aerobay.onrender.com/api/filter');
+    console.log('-')
+    filters.value = response.data;
+
   } catch (error) {
     console.error('Ошибка при загрузке подкатегорий:', error);
   }
 };
 
-console.log(subcategories.value)
 const uniqueSubcategories = computed(() => {
-  if (subcategories.value && Array.isArray(subcategories.value)) {
-    const map = new Map();
+  return groupBy(filter.filter_values, 'name');
+});
 
-    subcategories.value.forEach(subcategory => {
-      const name = subcategory.name;
-      const value = subcategory.value;
+// const submitForm = () => {
 
-      if (!map.has(name)) {
-        map.set(name, { name, subcategories: [] });
-      }
+// };
 
-      if (!map.get(name).subcategories.includes(value)) {
-        map.get(name).subcategories.push(value);
-      }
-    });
 
-    return Array.from(map.values());
-  }
-  return [];
+onMounted(async () => {
+  await fetchFilters();
+  uniqueSubcategories.value;
+  console.log(filters.value)
+
 });
 
 import { Popover, PopoverButton, PopoverPanel } from '@headlessui/vue'
@@ -83,6 +84,7 @@ import {
   FingerPrintIcon,
   SquaresPlusIcon,
 } from '@heroicons/vue/24/outline'
+import { keys } from "lodash";
 
 const solutions = [
   { name: 'Analytics', description: 'Get a better understanding of your traffic', href: '#', icon: ChartPieIcon },
@@ -96,7 +98,6 @@ const callsToAction = [
   { name: 'Contact sales', href: '#', icon: PhoneIcon },
 ]
 
-onMounted(fetchSubcategories);
 
 
 </script>
