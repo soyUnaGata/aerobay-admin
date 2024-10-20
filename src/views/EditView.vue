d19f6187070892ac3f1e5533e27f0b9bbef2ff22
soyUnaGata <fallen.snitch@gmail.com>
2024-10-20 19:39:46 +0300

    implement add btn method

--- src/views/EditView.vue
+++ src/views/EditView.vue
@@ -1,16 +1,16 @@
 <template>
-  <div>
+  <div class="w-full">
   <label for="filter" class="block text-sm font-medium leading-6 text-gray-900">
-    {{ filterDetails.filter.name }}
+    {{ filterDetails?.filter?.name }}
   </label>
 
   <div class="relative w-full flex gap-2.5 mt-2 rounded-md shadow-sm" v-for="(value, index) in filterDetails.filter.filter_values" :key="index">
-    <input type="text" name="filter" id="filter" :value="value.value" 
+    <input type="text" name="filter" id="filter" v-model="value.value"
            class="block rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 
                   placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" 
-           placeholder="Введите значение" />
+           placeholder="Enter value" />
 
-        <button v-if="index === filterDetails.filter.filter_values.length - 1" 
+        <button v-if="index === filterDetails.filter.filter_values.length - 1" @click="addNewValue()"
                 class="inset-y-0 justify-center right-0 flex items-center px-3 bg-green-500 hover:bg-green-600 active:bg-green-700 
                     text-white font-bold rounded-full w-8 h-8 shadow-lg transition transform hover:scale-110 active:scale-95">
         <span class="text-lg ">+</span>
@@ -28,6 +28,7 @@ import axios from 'axios';
 const route = useRoute();
 const filterId = ref(route.params.id); 
 const filterDetails = ref(null);
+const enteredValue = ref();
 
 const fetchGroupDetails = async () => {
   try {
@@ -37,6 +38,10 @@ const fetchGroupDetails = async () => {
     console.error('Ошибка при загрузке группы:', error);
   }
 };
+
+const addNewValue = () => {
+      filterDetails.value.filter.filter_values.push({ value: '' });
+    };
 
 watch(() => route.params.id,
       async (newId) => {
