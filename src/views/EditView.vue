ff34a40d3fab374ccaaf84425c59b7a8cda3329e
soyUnaGata <fallen.snitch@gmail.com>
2024-10-20 15:25:43 +0300

    amebd link

--- src/views/EditView.vue
+++ src/views/EditView.vue
@@ -1,21 +1,6 @@
 <template>
     <div>
-      <label for="price" class="block text-sm font-medium leading-6 text-gray-900">Price</label>
-      <div class="relative mt-2 rounded-md shadow-sm">
-        <div class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-          <span class="text-gray-500 sm:text-sm">$</span>
-        </div>
-        <input type="text" name="price" id="price" class="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="0.00" />
-        <div class="absolute inset-y-0 right-0 flex items-center">
-          <label for="currency" class="sr-only">Currency</label>
-          <select id="currency" name="currency" class="h-full rounded-md border-0 bg-transparent py-0 pl-2 pr-7 text-gray-500 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm">
-            <option>USD</option>
-          </select>
-        </div>
-      </div>
-    </div>
-    <div>
-      <label for="price" class="block bg-amber-200 text-sm font-medium leading-6 text-gray-900"></label>
+      <label for="price" class="block text-sm font-medium leading-6 text-gray-900">{{filterDetails.filter.name}}</label>
       <div class="relative mt-2 rounded-md shadow-sm">
         <div class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
           <span class="text-gray-500 sm:text-sm">$</span>
@@ -32,4 +17,40 @@
   </template>
 
 <script setup>
+import { ref, onMounted, watch } from 'vue';
+import { useRoute } from 'vue-router';
+import axios from 'axios';
+
+const route = useRoute();
+const filterId = ref(route.params.id); 
+const filterDetails = ref(null);
+
+const fetchGroupDetails = async () => {
+  try {
+    const response = await axios.get(`https://aerobay.onrender.com/api/filter/${filterId.value}`);
+    filterDetails.value = response.data;
+  } catch (error) {
+    console.error('Ошибка при загрузке группы:', error);
+  }
+};
+
+watch(
+      () => route.params.id,
+      async (newId) => {
+        if (newId) {
+          filterId.value = newId;
+          await fetchGroupDetails(); 
+        } else {
+          errorMessage.value = 'ID is not defined';
+        }
+      },
+      { immediate: true }
+    );
+
+
+onMounted(async() => {
+  await fetchGroupDetails();
+  filterDetails.value;
+  console.log(filterDetails.value);
+});
 </script>
