a50d9d5fcf3a3016d86aa58f6e4af4910321d74e
soyUnaGata <fallen.snitch@gmail.com>
2024-10-25 20:44:21 +0300

    implement back button

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,4 +1,7 @@
 <template>
+  <div>
+    <a :href="`/filter/add`">Add</a>
+  </div>
    <div class="flex" name="fade" v-for="(filter, index) in filters" :key="filter.id" :id="filter.id">
  <Popover :id="filter.id" class="relative shadow-lg ring-1 ring-gray-900/5 w-100 w-screen flex-auto overflow-hidden rounded-3xl bg-white text-sm leading-6 shadow-lg ring-1">
     <PopoverButton class="dropdown__filter inline-flex h-12 items-center gap-x-1 text-sm font-semibold leading-6 text-gray-900 pl-3">
@@ -7,22 +10,21 @@
       <ChevronDownIcon class="h-5 w-5" aria-hidden="true" />
     </PopoverButton>
     <RouterLink :to="`/filter/edit/${filter.id}`">Edit</RouterLink>
-    <button @click="removeFilter(filter.id, index)">Delete</button>
+    <button class="text-red-700" @click="removeFilter(filter.id, index)">Delete</button>
 
     <transition enter-active-class="transition ease-out duration-200" enter-from-class="opacity-0 translate-y-1" enter-to-class="opacity-100 translate-y-0" leave-active-class="transition ease-in duration-150" leave-from-class="opacity-100 translate-y-0" leave-to-class="opacity-0 translate-y-1">
       <PopoverPanel class="left-1/2 z-10 mt-5 flex w-screen max-w-max px-4">
         <div class="w-screen max-w-md flex-auto overflow-hidden rounded-3xl bg-white text-sm leading-6 ring-gray-900/5">
           <div class="p-4">
             <div v-for="(value, idx) in filter.filter_values" :key="idx" class="group relative flex gap-x-6 rounded-lg p-4 hover:bg-gray-50">
-              <div class="mt-1 flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
+              <!-- <div class="mt-1 flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
                 <component  class="h-6 w-6 text-gray-600 group-hover:text-indigo-600" aria-hidden="true" />
-              </div>
+              </div> -->
               <div>
-                <a :href="value.value" class="font-semibold text-gray-900">
+                <a  class="font-semibold text-gray-900">
                   {{ value.value }}
                   <span class="absolute inset-0" />
                 </a>
-                <p class="mt-1 text-gray-600">{{ }}</p>
               </div>
             </div>
           </div>
