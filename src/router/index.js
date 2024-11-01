3e14e9879aa73e384c8c4f01d05abd661eb6612b
soyUnaGata <fallen.snitch@gmail.com>
2024-11-01 14:34:12 +0200

    implement path for images

--- src/router/index.js
+++ src/router/index.js
@@ -1,47 +1,52 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    },
-    {
-      path: '/filter/edit/:id',
-      name: 'filter-edit',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/EditView.vue'),
-      props: true,
-    },
-    {
-      path: '/filter/add',
-      name: 'filter-add',
-      component: () => import('../views/AddView.vue')
-    },
-    {
-      path: '/accesories',
-      name: 'accessories',
-      component: () => import('../views/AccessoriesView.vue')
-    },
-    {
-      path: '/accesories/edit/:id',
-      name: 'accessories-edit',
-      component: () => import('../views/AcEditView.vue')
-    },
-  ]
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: HomeView
+        },
+        {
+            path: '/about',
+            name: 'about',
+            // route level code-splitting
+            // this generates a separate chunk (About.[hash].js) for this route
+            // which is lazy-loaded when the route is visited.
+            component: () => import('../views/AboutView.vue')
+        },
+        {
+            path: '/filter/edit/:id',
+            name: 'filter-edit',
+            // route level code-splitting
+            // this generates a separate chunk (About.[hash].js) for this route
+            // which is lazy-loaded when the route is visited.
+            component: () => import('../views/EditView.vue'),
+            props: true,
+        },
+        {
+            path: '/filter/add',
+            name: 'filter-add',
+            component: () => import('../views/AddView.vue')
+        },
+        {
+            path: '/accesories',
+            name: 'accessories',
+            component: () => import('../views/AccessoriesView.vue')
+        },
+        {
+            path: '/accesories/edit/:id',
+            name: 'accessories-edit',
+            component: () => import('../views/AcEditView.vue')
+        },
+        {
+            path: '/images',
+            name: 'images',
+            component: () => import('../views/ImageView.vue')
+        },
+    ]
 })
 
 export default router
