7f336ffd88d7f5c0800294948dbfd8317da835e9
soyUnaGata <fallen.snitch@gmail.com>
2024-11-03 17:56:11 +0200

    implement select for categories

--- src/components/CategorySelect.vue
+++ src/components/CategorySelect.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="category-select relative mb-4">
-    <label class="block text-sm font-medium text-gray-700 mb-2">Category</label>
+    <!--    <label class="block text-sm font-medium text-gray-700 mb-2">Category</label>-->
     <div @click="toggleDropdown"
          class="relative cursor-pointer select-input w-full px-4 py-2 bg-white border border-gray-300 rounded-lg shadow-sm text-left focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500">
       <span v-if="selectedCategory" class="text-gray-700">{{ selectedCategory.name }}</span>
