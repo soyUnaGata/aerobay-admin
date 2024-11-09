85bcb095d36234933648ca6d8aa649fd3bda2363
soyUnaGata <fallen.snitch@gmail.com>
2024-11-09 16:05:41 +0200

    implement view for editing accessory

---
+++ src/views/AcAddView.vue
@@ -0,0 +1,254 @@
+<template>
+  <nav class="fixed left-0 top-0 h-screen w-64 bg-gray-800 text-white">
+    <NavBar/>
+  </nav>
+  <div class="ml-64 flex-1 p-4 w-full">
+    <Loader v-if="loading"/>
+    <h2 class="text-xl font-semibold">Добавление аксессуара</h2>
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-4 border p-4 rounded-lg shadow-md">
+      <div class="flex flex-col gap-2.5">
+        <div class="space-y-2">
+          <label class="block text-gray-700">Название</label>
+          <input
+              v-model="accessoryDetails.title"
+              class="input-field w-full"
+              type="text"
+              placeholder="Название аксессуара"
+          />
+        </div>
+
+        <div class="space-y-2 col-span-2">
+          <label class="block text-gray-700">Описание</label>
+          <editor
+              api-key="odcydkl28d7x03wgsip6dxzkqtcx5olxt496s6x1nu87870j"
+              v-model="accessoryDetails.description"
+              :init="editorOptions"
+              class="w-full border border-gray-300 rounded-md"
+          />
+        </div>
+
+        <div class="space-y-2">
+          <label class="block text-gray-700">Цена</label>
+          <input
+              v-model="accessoryDetails.price"
+              class="input-field w-full"
+              type="number"
+              placeholder="Цена аксессуара"
+          />
+        </div>
+
+        <div class="space-y-2">
+          <label class="block text-gray-700">Скидка (%)</label>
+          <input
+              v-model="accessoryDetails.discount"
+              class="input-field w-full"
+              type="number"
+              placeholder="Скидка в процентах"
+          />
+        </div>
+
+        <div class="space-y-2">
+          <label class="block text-gray-700">URL изображения</label>
+          <input
+              v-model="accessoryDetails.image_url"
+              class="input-field w-full"
+              type="text"
+              placeholder="URL изображения"
+          />
+        </div>
+
+      </div>
+
+      <div class="flex flex-col gap-2.5">
+        <div class="space-y-2">
+          <label class="block text-gray-700">Размеры</label>
+          <input
+              v-model="accessoryDetails.dimensions"
+              class="input-field w-full"
+              type="text"
+              placeholder="Размеры аксессуара"
+          />
+        </div>
+
+        <div class="space-y-2">
+          <label class="block text-gray-700">Вес</label>
+          <input
+              v-model="accessoryDetails.weight"
+              class="input-field w-full"
+              type="number"
+              placeholder="Вес аксессуара"
+          />
+        </div>
+
+        <div class="space-y-2">
+          <label class="block text-gray-700">Тип</label>
+          <input
+              v-model="accessoryDetails.type"
+              class="input-field w-full"
+              type="text"
+              placeholder="Тип аксессуара"
+          />
+        </div>
+
+        <div class="space-y-2">
+          <label class="block text-gray-700">Количество на складе</label>
+          <input
+              v-model="accessoryDetails.amount"
+              class="input-field w-full"
+              type="number"
+              placeholder="Количество"
+          />
+        </div>
+
+        <div class="space-y-2">
+          <label class="block text-gray-700">Производитель</label>
+          <ManufacturerSelect
+              v-model="accessoryDetails.manufacturer_id"
+          />
+        </div>
+
+        <div class="space-y-2">
+          <label class="block text-gray-700">Категория</label>
+          <CategorySelect
+              v-model="accessoryDetails.category_id"
+          />
+        </div>
+
+        <div class="space-y-2">
+          <label class="block text-gray-700">Фильтры</label>
+          <Multiselect
+              v-model="selectedFilters"
+              :options="filters"
+              :multiple="true"
+              label="name"
+              placeholder="Выберите фильтры"
+          />
+        </div>
+
+        <div class="space-y-2">
+          <label class="block text-gray-700">Главное изображение</label>
+          <ImageSelector
+              :images="availableImages"
+              v-model:selectedImages="selectedImageIds"
+          />
+        </div>
+      </div>
+
+      <div class="col-span-2">
+        <button @click="addAccessory" class="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
+          Добавить
+        </button>
+        <RouterLink to="/accessories" class="ml-4 text-blue-500 hover:underline">Назад</RouterLink>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import {onMounted, ref, toRaw} from 'vue';
+import axios from "axios";
+import Multiselect from '../components/Multiselect.vue';
+import ManufacturerSelect from '../components/ManufacturerSelect.vue';
+import CategorySelect from "@/components/CategorySelect.vue";
+import ImageSelector from "@/components/ImageSelector.vue";
+import Editor from '@tinymce/tinymce-vue';
+import Loader from "@/components/Loader.vue";
+import NavBar from "@/components/NavBar.vue";
+
+const accessoryDetails = ref({
+  title: '',
+  description: '',
+  price: null,
+  discount: null,
+  image_url: '',
+  dimensions: '',
+  weight: '',
+  type: '',
+  amount: null,
+  manufacturer_id: null,
+  category_id: null,
+  subcategories: [],
+  images: [],
+  filter_values: []
+});
+
+const filters = ref([]);
+const selectedFilters = ref([]);
+const availableImages = ref([]);
+const selectedImageIds = ref([]);
+const loading = ref(false);
+
+const fetchFilters = async () => {
+  try {
+    const response = await axios.get('https://aerobay.onrender.com/api/filter_values');
+    filters.value = response.data || [];
+  } catch (error) {
+    console.error('Ошибка при загрузке подкатегорий:', error);
+  }
+};
+
+const fetchManufacturer = async () => {
+  try {
+    const response = await axios.get(`https://aerobay.onrender.com/api/manufactures`);
+    manufacturers.value = response.data.data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+const addAccessory = async () => {
+  try {
+    const newAccessory = {
+      ...accessoryDetails.value,
+      description: cleanHTML(accessoryDetails.value.description),
+      images: selectedImageIds.value,
+      subcategories: toRaw(accessoryDetails.value.subcategories.map(s => s.id)),
+      filter_values: selectedFilters.value.map(f => f.id)
+    };
+    await axios.post(`https://aerobay.onrender.com/api/accessories`, newAccessory);
+    alert('Аксессуар успешно добавлен');
+  } catch (error) {
+    console.error('Ошибка при добавлении аксессуара:', error);
+    alert('Ошибка при добавлении аксессуара');
+  }
+};
+
+const images = async () => {
+  try {
+    const response = await axios.get(`https://aerobay.onrender.com/api/images`);
+    availableImages.value = response.data.data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+onMounted(async () => {
+  await fetchFilters();
+  await fetchManufacturer();
+  await images();
+});
+</script>
+
+<style scoped>
+.input-field {
+  width: 100%;
+  padding: 0.5rem;
+  border: 1px solid #d1d5db;
+  border-radius: 0.375rem;
+  transition: border-color 0.2s;
+}
+
+.input-field:focus {
+  outline: none;
+  border-color: #2563eb;
+  box-shadow: 0 0 0 2px rgba(37, 99, 235, 0.4);
+}
+
+.ql-container {
+  height: 250px;
+}
+
+.margin-top {
+  margin-top: 0;
+}
+</style>
