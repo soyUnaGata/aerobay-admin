aac7c910d087f1034931d80b801c0d9957e4d879
soyUnaGata <fallen.snitch@gmail.com>
2024-12-12 18:56:08 +0200

    remove unused elements

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,9 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import {RouterView} from 'vue-router'
 </script>
 
 <template>
-  <header>
-    <div class="wrapper">
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <RouterView/>
 </template>
 
 <style scoped>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import NavBar from "@/components/NavBar.vue";
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <nav-bar></nav-bar>
   </main>
 </template>
