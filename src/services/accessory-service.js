3171277b6cd41b8246a9dec9aab30be35dffd482
soyUnaGata <fallen.snitch@gmail.com>
2024-12-16 18:32:09 +0200

    implement accessory service

---
+++ src/services/accessory-service.js
@@ -0,0 +1,25 @@
+import api from "@/services/api.js";
+
+class AccessoryService {
+    async getAllAccessories() {
+        return await api.get('accessories')
+            .then((response) => response.data.accessories)
+            .catch((err) => null);
+    }
+
+    async postAccessory(id, accessory) {
+        return await api.post(`accessories/${id}`, accessory);
+    }
+
+    async getAccessory(id) {
+        return await api.get(`accessories/${id}`)
+            .then((response) => response.data.accessory)
+            .catch((err) => null);
+    }
+
+    async removeAccessory(id) {
+        return await api.delete(`accessories/${id}`);
+    }
+}
+
+export default new AccessoryService();
