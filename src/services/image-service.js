5dd26332cf510fe5d45957c53c4e8af46e4c5863
soyUnaGata <fallen.snitch@gmail.com>
2024-12-24 20:08:31 +0200

    implement update methods

--- src/services/image-service.js
+++ src/services/image-service.js
@@ -7,8 +7,20 @@ class ImageService {
             .catch((err) => null);
     }
 
+    async getImage(id) {
+        return await api.get(`images/${id}`)
+            .then(res => res.data.image)
+    }
+
     async postImages(name, value) {
         return await api.put('images', {
+            name: name,
+            url: value,
+        })
+    }
+
+    async updateImage(id, name, value) {
+        return await api.put(`images/${id}`, {
             name: name,
             url: value,
         })
