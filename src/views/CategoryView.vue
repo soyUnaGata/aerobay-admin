4ee0e74ae9c20dd1cf2d9432d177dfc672b2a56e
soyUnaGata <fallen.snitch@gmail.com>
2025-05-11 19:39:00 +0300

    update modal and loading screen

--- src/views/CategoryView.vue
+++ src/views/CategoryView.vue
@@ -1,53 +1,53 @@
 <template>
-  <div class="flex">
-    <nav class="fixed left-0 top-0 h-screen w-64 bg-gray-800 text-white">
-      <NavBar/>
-    </nav>
+  <nav class="fixed left-0 top-0 h-screen w-64 bg-gray-800 text-white">
+    <NavBar/>
+  </nav>
+  <div class="w-screen">
+    <Loader v-if="loading"/>
+  </div>
+  <div class="flex-1 p-4 w-full" v-if="!loading">
     <div class="ml-64 flex-1 p-4 w-full">
-      <Loader v-if="loading"/>
-      <div class="ml-64 flex-1 p-4 w-full" v-if="!loading">
-        <!--        Implement modal-->
-        <div class="overflow-hidden rounded-lg shadow-lg w-full">
-          <table class="min-w-full bg-white border border-gray-200 rounded-lg">
-            <thead>
-            <tr class="bg-gray-50 text-left text-gray-600">
-              <th class="ppy-3 px-4 border-b font-semibold">#</th>
-              <th class="py-3 px-4 border-b font-semibold">Name</th>
-              <th class="py-3 px-4 border-b font-semibold">Actions</th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr
-                v-for="(category, index) in categories"
-                :key="category.id"
-                class="hover:bg-gray-100 transition duration-200 ease-in-out border-b"
-            >
-              <td class="ppy-3 px-4 text-gray-700 ">{{ index + 1 }}</td>
-              <td class="ppy-3 px-4 text-gray-700 w-5/6">{{ category.name }}</td>
-              <td class="ppy-3 px-4 space-x-2 flex w-1/6">
-                <button
-                    @click="editCategory(category.id)"
-                    class="bg-blue-500 hover:bg-blue-600 text-white py-1 px-3 rounded-full transition ease-in-out duration-150"
-                >
-                  Edit
-                </button>
-                <button
-                    @click="deleteCategory(category.id)"
-                    class="bg-red-500 hover:bg-red-600 text-white py-1 px-3 rounded-full transition ease-in-out duration-150"
-                >
-                  Delete
-                </button>
-              </td>
-            </tr>
-            </tbody>
-          </table>
-        </div>
+      <button @click="isOpen = true">Add</button>
+      <div class="overflow-hidden rounded-lg shadow-lg w-full">
+        <table class="min-w-full bg-white border border-gray-200 rounded-lg">
+          <thead>
+          <tr class="bg-gray-50 text-left text-gray-600">
+            <th class="ppy-3 px-4 border-b font-semibold">#</th>
+            <th class="py-3 px-4 border-b font-semibold">Name</th>
+            <th class="py-3 px-4 border-b font-semibold">Actions</th>
+          </tr>
+          </thead>
+          <tbody>
+          <tr
+              v-for="(category, index) in categories"
+              :key="category.id"
+              class="hover:bg-gray-100 transition duration-200 ease-in-out border-b"
+          >
+            <td class="ppy-3 px-4 text-gray-700 ">{{ index + 1 }}</td>
+            <td class="ppy-3 px-4 text-gray-700 w-5/6">{{ category.name }}</td>
+            <td class="ppy-3 px-4 space-x-2 flex w-1/6">
+              <button
+                  @click="editCategory(category.id)"
+                  class="bg-blue-500 hover:bg-blue-600 text-white py-1 px-3 rounded-full transition ease-in-out duration-150"
+              >
+                Edit
+              </button>
+              <button
+                  @click="deleteCategory(category.id)"
+                  class="bg-red-500 hover:bg-red-600 text-white py-1 px-3 rounded-full transition ease-in-out duration-150"
+              >
+                Delete
+              </button>
+            </td>
+          </tr>
+          </tbody>
+        </table>
       </div>
-      <Modal :title="'Category name'"
-             @save-item="saveCategory"
-             :isOpen="isOpen"
-             @close-modal="isOpen = false"/>
     </div>
+    <Modal :title="'Category name'"
+           @save-item="saveCategory"
+           :isOpen="isOpen"
+           @close-modal="isOpen = false"/>
   </div>
 </template>
 
@@ -58,9 +58,9 @@ import Loader from "@/components/Loader.vue";
 import CategoryService from "@/services/category-service.js";
 import Modal from "@/components/Modal.vue";
 
-const loading = ref(false);
+const loading = ref(true);
 const categories = ref([]);
-const isOpen = ref(true);
+const isOpen = ref(false);
 
 const fetchCategories = async () => {
   try {
@@ -68,6 +68,8 @@ const fetchCategories = async () => {
     console.log(categories.value);
   } catch (error) {
     console.log(error)
+  } finally {
+    loading.value = false;
   }
 };
 
@@ -76,12 +78,10 @@ const editCategory = (id) => {
 }
 
 const saveCategory = async (name) => {
-  console.log(name)
   const test = {
     name: name,
     description: ''
   }
-  console.log(test)
   try {
     await CategoryService.addCategory(test);
     isOpen.value = false;
