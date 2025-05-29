a027f104b23ac22917f653bbb509c212885d8dd4
soyUnaGata <fallen.snitch@gmail.com>
2025-05-29 17:42:19 +0300

    implement new methods for ManufacturesService

--- src/services/manufactures-service.js
+++ src/services/manufactures-service.js
@@ -6,6 +6,24 @@ class ManufacturesService {
             .then(res => res.data.data)
             .catch((err) => null);
     }
+
+    async addManufacturer(manufacture) {
+        return await api.post(`manufactures`, manufacture)
+    }
+
+    async updateManufacturer(id, category) {
+        return await api.put(`manufactures/${id}`, category);
+    }
+
+    async getManufacturer(id) {
+        return await api.get(`manufactures/${id}`)
+            .then((response) => response.data.data)
+            .catch((err) => null);
+    }
+
+    async removeManufacturer(id) {
+        return await api.delete(`manufactures/${id}`);
+    }
 }
 
 export default new ManufacturesService();
