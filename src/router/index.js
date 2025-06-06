80a1cb9f928184116e9f6d07b7a6e2109b8a6b98
soyUnaGata <fallen.snitch@gmail.com>
2025-06-06 21:05:43 +0300

    update base for github pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
-import {createRouter, createWebHashHistory} from 'vue-router'
+import {createRouter, createWebHashHistory, createWebHistory} from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
-    history: createWebHashHistory('/aerobay-admin'),
+    history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         {
             path: '/',
