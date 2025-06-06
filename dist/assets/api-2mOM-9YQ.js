797f9aa143bfa9ae596985d8b1bb4182c076603a
soyUnaGata <fallen.snitch@gmail.com>
2025-06-06 16:57:13 +0300

    Deploy to GitHub Pages

---
+++ dist/assets/api-2mOM-9YQ.js
@@ -0,0 +1,1 @@
+import{B as a}from"./main-C_11dBjN.js";const o=a.create({baseURL:"https://aerobay.onrender.com/api/",headers:{"content-type":"application/json"}});export{o as i};
---
+++ dist/assets/notFound-n_t2e_zJ.js
@@ -0,0 +1,1 @@
+import"./main-C_11dBjN.js";
