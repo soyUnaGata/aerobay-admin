f2eb12d8a3b1a0cead4ee82f238f36d2dc107f00
soyUnaGata <fallen.snitch@gmail.com>
2025-06-06 21:32:05 +0300

    Updates

--- assets/api-BKxU2iXZ.js
+++
@@ -1,1 +0,0 @@
-import{B as a}from"./main-BdTq_GBk.js";const o=a.create({baseURL:"https://aerobay.onrender.com/api/",headers:{"content-type":"application/json"}});export{o as i};
---
+++ assets/api-BLsDvcKO.js
@@ -0,0 +1,1 @@
+import{B as a}from"./main-DQ80nIDV.js";const o=a.create({baseURL:"https://aerobay.onrender.com/api/",headers:{"content-type":"application/json"}});export{o as i};
